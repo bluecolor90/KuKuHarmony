4e6bf24bdc674f8761bdfedea282e1d8a6da6e25
bluecolor90 <37956277+bluecolor90@users.noreply.github.com>
2019-07-15 01:54:43 +0900

    test
    test

--- devicetypes/turlvo/kuku-harmony-aircon.src/kuku-harmony-aircon.groovy
+++ devicetypes/turlvo/kuku-harmony-aircon.src/kuku-harmony-aircon.groovy
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+/*test*/
 metadata {
 	definition (name: "KuKu Harmony_Aircon", namespace: "turlvo", author: "KuKu") {
         capability "Actuator"
