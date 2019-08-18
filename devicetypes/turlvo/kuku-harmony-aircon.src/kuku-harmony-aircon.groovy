ead8689d409f21595d9f4ed0cae10fde7cb9a767
bluecolor90 <rlawjddn90@naver.com>
2019-08-18 14:33:43 +0900

    delaytest

--- devicetypes/turlvo/kuku-harmony-aircon.src/kuku-harmony-aircon.groovy
+++ devicetypes/turlvo/kuku-harmony-aircon.src/kuku-harmony-aircon.groovy
@@ -243,7 +243,7 @@ def coolwithlowtemp() {
     {
         log.debug "child>cooldwithlowtemp> debug, in loop ${it}"
         parent.command(this, "tempdown")
-        sleep(1)
+        Thread.sleep(100)
     }
     for(i=0;i<4;i++)
     {
